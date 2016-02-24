8b674659da7ae1bdf6e5c36dea27251cfaad7d0d
Daniel Reid <dreid1991@gmail.com>
2016-02-23 22:52:30 -0600

    dihedrals compile, need to test

--- FixDihedralOPLS.cu
+++ FixDihedralOPLS.cu
@@ -16,94 +16,189 @@ __global__ void compute_cu(int nAtoms, float4 *xs, float4 *forces, cudaTextureOb
         //startIdx - idxBeginCopy gives my index in shared memory
         int shr_idx = startIdx - idxBeginCopy;
         int n = endIdx - startIdx;
-        int idSelf = dihedrals_shr[startIdx].ids[dihedrals_shr[startIdx].myIdx];
+        if (n) {
+            int idSelf = dihedrals_shr[startIdx].ids[dihedrals_shr[startIdx].myIdx];
+            
+            int idxSelf = tex2D<int>(idToIdxs, XIDX(idSelf, sizeof(int)), YIDX(idSelf, sizeof(int)));
         
-        int idxSelf = tex2D<int>(idToIdxs, XIDX(idSelf, sizeof(int)), YIDX(idSelf, sizeof(int)));
-    
-        float3 pos = make_float3(xs[idxSelf]);
-        float3 forceSum = make_float3(0, 0, 0);
-        for (int i=0; i<n; i++) {
-            DihedralOPLSGPU dihedral= dihedrals_shr[shr_idx + i];
-            float3 positions[4];
-            positions[dihedral.myIdx] = pos;
-            int toGet[3];
-            if (dihedral.myIdx==0) {
-                toGet[0] = 1;
-                toGet[1] = 2;
-                toGet[2] = 3;
-            } else if (dihedral.myIdx==1) {
-                toGet[0] = 0;
-                toGet[1] = 2;
-                toGet[2] = 3;
-            } else if (dihedral.myIdx==2) {
-                toGet[0] = 0;
-                toGet[1] = 1;
-                toGet[2] = 3;
-            } else if (dihedral.myIdx==3) {
-                toGet[0] = 0;
-                toGet[1] = 1;
-                toGet[2] = 2;
-            }
-            for (int i=0; i<3; i++) {
-                positions[toGet[i]] = make_float3(perAtomFromId(idToIdxs, xs, dihedral.ids[toGet[i]]));
-            }
-            for (int i=1; i<3; i++) {
-                positions[i] = positions[0] + bounds.minImage(positions[i]-positions[0]);
-            }
-            float3 directors[3]; //vb_xyz in lammps
-            float lenSqrs[3];
-            float lens[3];
-            float invLenSqrs[3]; //sb in lammps
-            directors[0] = positions[0] - positions[1];
-            directors[1] = positions[2] - positions[1];
-            directors[2] = positions[3] - positions[2];
+            float3 pos = make_float3(xs[idxSelf]);
+            float3 forceSum = make_float3(0, 0, 0);
+            for (int i=0; i<n; i++) {
+                DihedralOPLSGPU dihedral = dihedrals_shr[shr_idx + i];
+                float3 positions[4];
+                positions[dihedral.myIdx] = pos;
+                int toGet[3];
+                if (dihedral.myIdx==0) {
+                    toGet[0] = 1;
+                    toGet[1] = 2;
+                    toGet[2] = 3;
+                } else if (dihedral.myIdx==1) {
+                    toGet[0] = 0;
+                    toGet[1] = 2;
+                    toGet[2] = 3;
+                } else if (dihedral.myIdx==2) {
+                    toGet[0] = 0;
+                    toGet[1] = 1;
+                    toGet[2] = 3;
+                } else if (dihedral.myIdx==3) {
+                    toGet[0] = 0;
+                    toGet[1] = 1;
+                    toGet[2] = 2;
+                }
+                for (int i=0; i<3; i++) {
+                    positions[toGet[i]] = make_float3(perAtomFromId(idToIdxs, xs, dihedral.ids[toGet[i]]));
+                }
+                for (int i=1; i<3; i++) {
+                    positions[i] = positions[0] + bounds.minImage(positions[i]-positions[0]);
+                }
+                float3 directors[3]; //vb_xyz in lammps
+                float lenSqrs[3]; //bnmag2 in lammps
+                float lens[3]; //bnmag in lammps
+                float invLenSqrs[3]; //sb in lammps
+                float invLens[3];
+                directors[0] = positions[0] - positions[1];
+                directors[1] = positions[2] - positions[1];
+                directors[2] = positions[3] - positions[2];
 
-            for (int i=0; i<3; i++) {
-                lenSqrs[i] = lengthSqr(directors[i]);
-                lens[i] = sqrtf(lenSqrs[i]);
-                invLenSqrs[i] = 1.0f / lenSqrs[i];
-            }
-
-            float invLenBonds13[2];
-            invLenBonds13[0] = 1.0f / lens[0];
-            invLenBonds13[1] = 1.0f / lens[2];
-
-            float c0 = dot(directors[0], directors[2]) * invLenBonds13[0] * invLenBonds13[1];
-            /*
+                for (int i=0; i<3; i++) {
+                    lenSqrs[i] = lengthSqr(directors[i]);
+                    lens[i] = sqrtf(lenSqrs[i]);
+                    invLenSqrs[i] = 1.0f / lenSqrs[i];
+                    invLens[i] = 1.0f / lens[i];
+                }
 
 
-            float distSqrs[2];
-            float dists[2];
-            for (int i=0; i<2; i++) {
-                distSqrs[i] = lengthSqr(directors[i]);
-                dists[i] = sqrtf(distSqrs[i]);
-            }
-            float c = dot(directors[0], directors[1]);
-            float invDistProd = 1.0f / (dists[0]*dists[1]);
-            c *= invDistProd;
-            if (c>1) {
-                c=1;
-            } else if (c<-1) {
-                c=-1;
-            }
-            float s = sqrtf(1-c*c);
-            float dTheta = acosf(c) - angle.thetaEq;
-            float forceConst = angle.k * dTheta;
-            float a = -2 * forceConst * s;
-            float a11 = a*c/distSqrs[0];
-            float a12 = -a*invDistProd;
-            float a22 = a*c/distSqrs[1];
+                float c0 = dot(directors[0], directors[2]) * invLens[0] * invLens[2];
+                float c12Mags[2];
+                float invMagProds[2]; //r12c1, 2 in lammps
+                for (int i=0; i<2; i++) {
+                    float dotProd = dot(directors[i+1], directors[i]);
+                    invMagProds[i] = invLens[i] * invLens[i+1];
+                    c12Mags[i] = dotProd * invMagProds[i]; //lammps variable names are opaque
+                }
 
-            if (angle.myIdx==0) {
-                forceSum += ((directors[0] * a11) + (directors[1] * a12)) * 0.5;
-            } else if (angle.myIdx==1) {
-                forceSum -= ((directors[0] * a11) + (directors[1] * a12) + (directors[1] * a22) + (directors[0] * a12)) * 0.5; 
-            } else {
-                forceSum += ((directors[1] * a22) + (directors[0] * a12)) * 0.5;
+                float scValues[3]; //???, is s1, s2, s12 in lammps
+                for (int i=0; i<2; i++) {
+                    float x = max(1 - c12Mags[i]*c12Mags[i], 0.0f);
+                    float sqrtVal = max(sqrtf(x), EPSILON);
+                    scValues[i] = 1.0 / sqrtVal;
+                }
+                scValues[2] = scValues[0] * scValues[1];
+
+                for (int i=0; i<2; i++) {
+                    scValues[i] *= scValues[i]; 
+                }
+                float c = (c0 + c12Mags[0]*c12Mags[1]) * scValues[2];
+
+                float3 cVector;
+                cVector.x = directors[0].y*directors[1].z - directors[0].z*directors[1].y;
+                cVector.y = directors[0].z*directors[1].x - directors[0].x*directors[1].z;
+                cVector.z = directors[0].x*directors[1].y - directors[0].y*directors[1].x;
+
+                float cVectorLen = length(cVector);
+                float dx = dot(cVector, directors[2]) * invLens[2] / cVectorLen;
+                if (c > 1.0f) {
+                    c = 1.0f;
+                } else if (c < -1.0f) {
+                    c = -1.0f;
+                }
+                float phi = acosf(c);
+                if (dx < 0) {
+                    phi = -phi;
+                }
+                float sinPhi = sinf(phi);
+                float absSinPhi = sinPhi < 0 ? -sinPhi : sinPhi;
+                if (absSinPhi < EPSILON) {
+                    sinPhi = EPSILON;
+                }
+                float invSinPhi = 1.0f / sinPhi;
+
+                float derivOfPotential = 
+                             dihedral.coefs[0] 
+                    - 2.0f * dihedral.coefs[1] * sinf(2.0f*phi) * invSinPhi
+                    + 3.0f * dihedral.coefs[2] * sinf(3.0f*phi) * invSinPhi
+                    - 4.0f * dihedral.coefs[3] * sinf(4.0f*phi) * invSinPhi
+                    ;
+
+                c *= derivOfPotential;
+                scValues[2] *= derivOfPotential;
+                float a11 = c * invLenSqrs[0] * scValues[0];
+                float a22 = -invLenSqrs[1] * (2.0f*c0*scValues[2] - c*(scValues[0]*scValues[1]));
+                float a33 = c*invLenSqrs[2]*scValues[1];
+                float a12 = -invMagProds[0] * (c12Mags[0] * c * scValues[0] + c12Mags[1] * scValues[2]);
+                float a13 = -invLens[0] * invLens[2] * scValues[2];
+                float a23 = invMagProds[1] * (c12Mags[1]*c*scValues[1] + c12Mags[0]*scValues[2]);
+                float3 myForce = make_float3(0, 0, 0);
+                float3 sFloat3 = make_float3(
+                        a12*directors[0].x + a22*directors[1].x + a23*directors[2].x
+                        ,  a12*directors[0].y + a22*directors[1].y + a23*directors[2].y
+                        ,  a12*directors[0].z + a22*directors[1].z + a23*directors[2].z
+                        );
+                if (dihedral.myIdx <= 1) {
+                    float3 a11Dir1 = directors[0] * a11;
+                    float3 a12Dir2 = directors[1] * a12;
+                    float3 a13Dir3 = directors[2] * a13;
+                    myForce.x += a11Dir1.x + a12Dir2.x + a13Dir3.x;
+                    myForce.y += a11Dir1.y + a12Dir2.y + a13Dir3.y;
+                    myForce.z += a11Dir1.z + a12Dir2.z + a13Dir3.z;
+
+                    if (dihedral.myIdx == 1) {
+                        
+                        myForce = -sFloat3 - myForce;
+                    }
+                } else {
+                    float3 a13Dir1 = directors[0] * a13;
+                    float3 a23Dir2 = directors[1] * a23;
+                    float3 a33Dir3 = directors[2] * a33;
+                    myForce.x += a13Dir1.x + a23Dir2.x + a33Dir3.x;
+                    myForce.y += a13Dir1.y + a23Dir2.y + a33Dir3.y;
+                    myForce.z += a13Dir1.z + a23Dir2.z + a33Dir3.z;
+                    if (dihedral.myIdx == 2) {
+                        myForce = sFloat3 - myForce;
+
+                    }
+
+
+                }
+                forceSum += myForce;
+
+
+                /*
+
+
+                float distSqrs[2];
+                float dists[2];
+                for (int i=0; i<2; i++) {
+                    distSqrs[i] = lengthSqr(directors[i]);
+                    dists[i] = sqrtf(distSqrs[i]);
+                }
+                float c = dot(directors[0], directors[1]);
+                float invDistProd = 1.0f / (dists[0]*dists[1]);
+                c *= invDistProd;
+                if (c>1) {
+                    c=1;
+                } else if (c<-1) {
+                    c=-1;
+                }
+                float s = sqrtf(1-c*c);
+                float dTheta = acosf(c) - angle.thetaEq;
+                float forceConst = angle.k * dTheta;
+                float a = -2 * forceConst * s;
+                float a11 = a*c/distSqrs[0];
+                float a12 = -a*invDistProd;
+                float a22 = a*c/distSqrs[1];
+
+                if (angle.myIdx==0) {
+                    forceSum += ((directors[0] * a11) + (directors[1] * a12)) * 0.5;
+                } else if (angle.myIdx==1) {
+                    forceSum -= ((directors[0] * a11) + (directors[1] * a12) + (directors[1] * a22) + (directors[0] * a12)) * 0.5; 
+                } else {
+                    forceSum += ((directors[1] * a22) + (directors[0] * a12)) * 0.5;
+                }
+                */
             }
-            */
+            forces[idxSelf] += forceSum;
         }
-        forces[idxSelf] += forceSum;
     }
 }
 
