3d9c049e96ede9e6e58f88704f4705bc894d55d9
Julian Helfferich <jhelfferich@uchicago.edu>
2016-03-03 14:34:17 -0600

    Add documentation for GPUArrayBase

--- GPUArrayBase.h
+++ GPUArrayBase.h
@@ -1,13 +1,24 @@
 #ifndef GPUARRAYBASE_H
 #define GPUARRAYBASE_H
+
 #include <vector>
+
 using namespace std;
 
+/*! \class GPUArrayBase
+ * \brief Base class for a GPUArray
+ */
 class GPUArrayBase {
     public:
-        int size;
-        virtual void dataToDevice(){};
-        virtual void dataToHost(){};
+        /*! \brief Constructor */
         GPUArrayBase(){};
+
+        /*! \brief Send data from host to GPU device */
+        virtual void dataToDevice(){};
+
+        /*! \brief Send data from GPU device to host */
+        virtual void dataToHost(){};
+
+        int size; //!< Size of the array
 };
 #endif
