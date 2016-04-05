2ec88a5df0bac97ba777473043aede1a92461638
Julian Helfferich <jhelfferich@uchicago.edu>
2016-04-05 16:20:59 -0500

    Include globalDefs inside include guards, shorten very long lines

--- GPUArrayTexBase.h
+++ GPUArrayTexBase.h
@@ -1,15 +1,21 @@
-#include "globalDefs.h"
-
 #ifndef GPUARRAYTEXBASE_H
 #define GPUARRAYTEXBASE_H
 
-#define SAFEHOSTCAPACITY(n, SIZE) (n <= (PERLINE/sizeof(SIZE)) ? n : (PERLINE/sizeof(SIZE)) * ceil(n/(float)(PERLINE/sizeof(SIZE))))
-//inheriting as a template is behaving strangely.  Going to make it deal with voidssss
+#include "globalDefs.h"
+
+#define SAFEHOSTCAPACITY(n, SIZE) \
+    (n <= (PERLINE/sizeof(SIZE)) ? n : \
+                (PERLINE/sizeof(SIZE))*ceil(n/(float)(PERLINE/sizeof(SIZE))))
+
 class GPUArrayTexBase {
     protected:
-        void copyToDeviceArrayInternal(void *dest, cudaArray *src, int numBytes) {
-            //MAKE SURE THIS WORKS FOR COPYING FROM 2d ARRAYS
-            CUCHECK(cudaMemcpyFromArray(dest, src, 0, 0, numBytes, cudaMemcpyDeviceToDevice));
+        void copyToDeviceArrayInternal(void *dest,
+                                       cudaArray *src,
+                                       int numBytes)
+        {
+            //! \todo Make sure this works for copying from 2d arrays
+            CUCHECK(cudaMemcpyFromArray(dest, src, 0, 0, numBytes,
+                                                    cudaMemcpyDeviceToDevice));
         }
 
     public:
