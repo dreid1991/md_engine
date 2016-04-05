ed3845d8ea09d7be9a2391378d2a8257080d74e1
Julian Helfferich <jhelfferich@uchicago.edu>
2016-04-05 15:22:28 -0500

    Add documentation for Integrater

--- Integrater.h
+++ Integrater.h
@@ -15,39 +15,126 @@ class State;
 extern const string IntVerletType;
 extern const string IntRelaxType;
 
+/*! \class Integrater
+ * \brief Base class for Molecular Dynamics Integraters
+ *
+ * This class is a base class for all MD Integraters. It takes care of all
+ * aspects common to all integrators such as doing basic checks, data transfer
+ * from and to the GPU, etc.
+ */
 class Integrater {
 
-    protected:
-        //virtual void preForce(uint);
-        void force(bool);
-        //virtual void postForce(uint);
-        void asyncOperations();
-        std::vector<GPUArrayBase *> activeData;
-        void basicPreRunChecks();
-        void basicPrepare(int);
-        void basicFinish();
-        void setActiveData();
-        void doDataCollection();
-        void singlePointEng(); //make a python-wrapped version
-        public:
-            double singlePointEngPythonAvg(string groupHandle);
-            boost::python::list singlePointEngPythonPerParticle();
-            string type;
-            State *state;
-            Integrater() {};
-            Integrater(State *state_, string type_);
+protected:
+    /*! \brief Calculate force for all fixes
+     *
+     * \param computeVirials Compute virials for all forces if True
+     *
+     * This function iterates over all fixes and if the Fix should be applied
+     * its force (and virials) is computed.
+     */
+    void force(bool computeVirials);
     
-            //double relax(int numTurns, num fTol);
-            void forceSingle(bool);
-            void writeOutput();
-/*	void verletPreForce(vector<Atom *> &atoms, double timestep);
-	void verletPostForce(vector<Atom *> &atoms, double timestep);
-	void compute(vector<Fix *> &, int);
-	void firstTurn(Run &params);
-	void run(Run &params, int currentTurn, int numTurns);
-	bool rebuildIsDangerous(vector<Atom *> &atoms, double);
-	void addKineticEnergy(vector<Atom *> &, Data &);
-	void setThermoValues(Run &);*/
+    /*!\ brief Perform all asynchronous operations
+     *
+     * This function performs all asynchronous operations, such as writing
+     * configurations or performing Python operations
+     */
+    void asyncOperations();
+    std::vector<GPUArrayBase *> activeData; //!< List of pointers to the data
+                                            //!< used by this integrator
+
+    /*! \brief Simple checks before the run
+     *
+     * The checks consist of:
+     *   - GPU device compatibility needs to be >= 3.0
+     *   - Atom grid needs to be set
+     *   - Cutoff distance needs to be set
+     *   - 2d system may not be periodic in z dimension
+     *   - Grid discretization must not be smaller than cutoff distance plus
+     *     padding
+     */
+    void basicPreRunChecks();
+
+    /*! \brief Prepare Integrator for running
+     *
+     * \param numTurns Number of turns the integrator is expected to run
+     *
+     * Prepare the integrator to run for a given amount of timesteps. This
+     * includes copying all data to the GPU device and calling prepareForRun()
+     * on all fixes.
+     */
+    void basicPrepare(int numTurns);
+
+    /*! \brief Finish simulation run
+     *
+     * Finish the simulation run. This includes copying all relevant data to
+     * the CPU host and calling postRun on all fixes.
+     */
+    void basicFinish();
+
+    /*! \brief Collect all pointers to the relevant data into activeData */
+    void setActiveData();
+
+    /*! \brief Collect data for all DataSets*/
+    void doDataCollection();
+
+    /*! \brief Calculate single point energy for all fixes
+     *
+     * A single point energy excludes energy/forces from thermostat fixes and
+     * the likes.
+     */
+    void singlePointEng(); //make a python-wrapped version
+public:
+
+    /*! \brief Calculate and return single point energy
+     *
+     * \param groupHandle Handle defining the group used for averaging
+     *
+     * \return Average energy for all particles in the specified group
+     *
+     * This function calculates and returns the average per particle energy for
+     * the particles in the group specified via the groupHandle.
+     */
+    double singlePointEngPythonAvg(string groupHandle);
+
+    /*! \brief Create list of per-particle energies
+     *
+     * \return List containing the per-particle energy for each atom
+     *
+     * This function calculates the per-particle energy and returns a list
+     * containing one value per atom in the simulation.
+     */
+    boost::python::list singlePointEngPythonPerParticle();
+    string type; //!< Unused \todo remove
+    State *state; //!< Pointer to the simulation state
+
+    /*! \brief Default constructor
+     *
+     * \todo Do we need a default constructor?
+     */
+    Integrater() {};
+
+    /*! \brief Constructor
+     *
+     * \param state_ Pointer to simulation state
+     * \param type_ String specifying type of Integrater (unused)
+     *
+     * \todo Remove usage of type
+     */
+    Integrater(State *state_, string type_);
+    //double relax(int numTurns, num fTol);
+
+    /*! \brief Calculate single point force
+     *
+     * \param computeVirials Virials are computed if this parameter is True
+     *
+     * Calculate single point energy, i.e. calculate energy only for \link Fix
+     * Fixes \endlink with Fix::forceSingle == True.
+     */
+    void forceSingle(bool computeVirials);
+
+    /*! \brief Write output for all \link WriteConfig WriteConfigs \endlink */
+    void writeOutput();
 };
 
 
