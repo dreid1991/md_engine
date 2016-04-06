593e101a96b9e72e08783df1204c8e6c459d608f
Julian Helfferich <jhelfferich@uchicago.edu>
2016-04-06 06:25:24 -0500

    Add documentation for GPUArrayTexBase

--- GPUArrayTexBase.h
+++ GPUArrayTexBase.h
@@ -3,8 +3,21 @@
 
 #include "globalDefs.h"
 
+/*! \brief Base class for GPU Texture Arrays
+ *
+ * This class is a base class to work with Texture arrays.
+ */
 class GPUArrayTexBase {
     protected:
+        /*! \brief Copy data from host device to the GPU texture
+         *
+         * \param dest Destination: Pointer to the GPU memory
+         * \param src Source: Pointer to cudaArray
+         * \param numBytes Number of bytes to be copied
+         *
+         * This function copies data from the GPU device onto a GPU texture
+         * array.
+         */
         void copyToDeviceArrayInternal(void *dest,
                                        cudaArray *src,
                                        int numBytes)
@@ -15,7 +28,11 @@ class GPUArrayTexBase {
         }
 
     public:
+        /*! \brief Copy data from GPU device to texture
+         *
+         * \param dest Destination: Pointer to the GPU memory
+         */
         virtual void copyToDeviceArray(void *dest){};
-        cudaChannelFormatDesc channelDesc;
+        cudaChannelFormatDesc channelDesc; //!< Descriptor for the texture
 };
 #endif
