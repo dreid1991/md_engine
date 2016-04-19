dbadeae3f97749bcda2a7811e837cf5d3ba9880f
Julian Helfferich <jhelfferich@uchicago.edu>
2016-04-19 05:50:49 -0500

    I learned how to make doxygen comments look nicer
    Using //! you do not need \brief. This makes the comments in the
    code more readable.

--- GPUArrayDeviceGlobal.h
+++ GPUArrayDeviceGlobal.h
@@ -8,11 +8,17 @@
 #include "GPUArrayDevice.h"
 #include "Logging.h"
 
-/*! \brief Global function to set the device memory */
-void MEMSETFUNC(void *, const void *, size_t, size_t);
+//! Global function to set the device memory
+/*!
+ * \param ptr Pointer to the memory
+ * \param val Pointer to the value that will be stored into all elements
+ * \param n Number of elements that will be set to val
+ * \param Tsize Size of the value type
+ */
+void MEMSETFUNC(void *ptr, const void *val, size_t n, size_t Tsize);
 
-/*! \brief Array on the GPU device
- *
+//!Array on the GPU device
+/*!
  * \tparam T Data type stored in the array
  *
  * Array storing data on the GPU device.
@@ -20,9 +26,9 @@ void MEMSETFUNC(void *, const void *, size_t, size_t);
 template <typename T>
 class GPUArrayDeviceGlobal : public GPUArrayDevice {
 public:
-    /*! \brief Constructor
-     *
-     * \param n_ Size of the array (number of elements)
+    //! Constructor
+    /*!
+     * \param size Size of the array (number of elements)
      *
      * This constructor creates the array on the GPU device and allocates
      * enough memory to store n_ elements.
@@ -30,7 +36,7 @@ public:
     explicit GPUArrayDeviceGlobal(size_t size = 0)
         : GPUArrayDevice(size) { allocate(); }
 
-    /*! \brief Copy constructor */
+    //! Copy constructor
     GPUArrayDeviceGlobal(const GPUArrayDeviceGlobal<T> &other)
         : GPUArrayDevice(other.n)
     {
@@ -39,7 +45,7 @@ public:
                                                 cudaMemcpyDeviceToDevice));
     }
 
-    /*! \brief Move constructor */
+    //! Move constructor
     GPUArrayDeviceGlobal(GPUArrayDeviceGlobal<T> &&other)
         : GPUArrayDevice(other.n)
     {
@@ -49,12 +55,16 @@ public:
         other.ptr = nullptr;
     }
 
-    /*! \brief Destructor */
+    //! Destructor
     ~GPUArrayDeviceGlobal() {
         deallocate();
     }
 
-    /*! \brief Assignment operator */
+    //! Assignment operator
+    /*!
+     * \param other GPUArrayDeviceGlobal from which data is copied
+     * \return This object
+     */
     GPUArrayDeviceGlobal<T> &operator=(const GPUArrayDeviceGlobal<T> &other) {
         if (n != other.n) {
             //! \todo Think about if it would be better not to force
@@ -66,7 +76,7 @@ public:
         return *this;
     }
 
-    /*! \brief Move assignment operator */
+    //! Move assignment operator
     GPUArrayDeviceGlobal<T> &operator=(GPUArrayDeviceGlobal<T> &&other) {
         deallocate();
         n = other.n;
@@ -77,21 +87,23 @@ public:
         return *this;
     }
 
-    /*! \brief Access pointer to data
-     *
+    //! Access pointer to data
+    /*!
      * \return Pointer to memory location
      */
     T *data() { return (T*)ptr; }
 
-    /* \brief Const access pointer to data
-     *
+    //! Const access pointer to data
+    /*!
      * \return Const pointer to memory location
      */
     T const *data() const { return (T const*)ptr; }
 
-    /*! \brief Copy data to given pointer
-     *
+    //! Copy data to given pointer
+    /*!
      * \param copyTo Pointer to the memory to where the data will be copied
+     *
+     * \return Pointer to CPU memory location where data was copied to
      *
      * This function copies the data stored in the GPU array to the
      * position specified by the pointer *copyTo using cudaMemcpy
@@ -104,6 +116,15 @@ public:
                                                 cudaMemcpyDeviceToHost));
         return copyTo;
     }
+
+    //! Copy a given amount of data from a specific position of the array
+    /*!
+     * \param copyTo CPU Memory location where the data is copied to
+     * \param offset Initial array index to be copied (0 is first element)
+     * \param num Number of elements to be copied
+     *
+     * \return Pointer to memory location where data was copied to
+     */
     T *getWithOffset(T *copyTo, int offset, int num) {
         if (copyTo == (T *) NULL) {
             copyTo = (T *) malloc(n*sizeof(T));
@@ -114,10 +135,12 @@ public:
         return copyTo;
     }
 
-    /*! \brief Copy data to pointer asynchronously
-     *
+    //! Copy data to pointer asynchronously
+    /*!
      * \param copyTo Pointer to the memory where the data will be copied to
      * \param stream cudaStream_t object used for asynchronous copy
+     *
+     * \return Pointer to CPU memory location where data was copied to
      *
      * Copy data stored in the GPU array to the address specified by the
      * pointer copyTo using cudaMemcpyAsync.
@@ -131,8 +154,8 @@ public:
         return copyTo;
     }
 
-    /*! \brief Copy data from pointer
-     *
+    //! Copy data from pointer
+    /*!
      * \param copyFrom Pointer to address from which the data will be
      *                 copied
      *
@@ -145,8 +168,8 @@ public:
                                                 cudaMemcpyHostToDevice));
     }
 
-    /*! \brief Copy data to pointer
-     *
+    //! Copy data to pointer
+    /*!
      * \param dest Pointer to the memory to which the data should be copied
      *
      * Copy data from the device to the memory specified by the pointer
@@ -160,8 +183,8 @@ public:
                                                 cudaMemcpyDeviceToDevice));
     }
 
-    /*! \brief Copy data to pointer asynchronously
-     *
+    //! Copy data to pointer asynchronously
+    /*!
      * \param dest Pointer to the memory to which the data should be copied
      * \param stream cudaStream_t object for asynchronous copy
      *
@@ -176,8 +199,8 @@ public:
                                         cudaMemcpyDeviceToDevice, stream));
     }
 
-    /*! \brief Set all bytes in the array to a specific value
-     *
+    //! Set all bytes in the array to a specific value
+    /*!
      * \param val Value written to all bytes in the array
      *
      * WARNING: val must be a one byte value
@@ -191,8 +214,8 @@ public:
         CUCHECK(cudaMemset(ptr, val, n*sizeof(T)));
     }
 
-    /*! \brief Set array elements to a specific value
-     *
+    //! Set array elements to a specific value
+    /*!
      * \param val Value the elements are set to
      *
      * Set all array elements to the value specified by the parameter val
@@ -205,10 +228,10 @@ public:
     }
 
 private:
-    /*! \brief Allocate memory */
+    //! Allocate memory
     void allocate() { CUCHECK(cudaMalloc(&ptr, n * sizeof(T))); cap = size(); }
 
-    /*! \brief Deallocate memory */
+    //! Deallocate memory
     void deallocate() {
         CUCHECK(cudaFree(ptr));
         n = 0;
