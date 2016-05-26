72373e72ac6d184745dbe813038931b3deeebdeb
Julian Helfferich <julian.helfferich@googlemail.com>
2016-05-26 14:36:41 -0500

    Fix error in Fix2d constructor
    The const string Fix2dType has to be a global variable. Otherwise,
    the Fix constructor is called before it is initialized, leading to
    a nasty segfault.

--- src/Fixes/Fix2d.h
+++ src/Fixes/Fix2d.h
@@ -6,11 +6,10 @@
 
 class State;
 
+const std::string _2dType = "2d";
+
 void export_Fix2d();
 class Fix2d : public Fix {
-
-private:
-    const std::string _2dType = "2d";
 
 public:
     Fix2d(boost::shared_ptr<State> state_, std::string handle_, int applyEvery_)
