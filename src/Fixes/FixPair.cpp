2545414dca7268aeae59f58bbbb4d0d94290805e
Julian Helfferich <julian.helfferich@googlemail.com>
2016-06-25 05:46:05 -0500

    Reduce usage of "using namespace"
    I hope this will in the end resolve our strange import error. Until
    then, it is simply good practice.

--- src/Fixes/FixPair.cpp
+++ src/Fixes/FixPair.cpp
@@ -4,12 +4,16 @@
 
 #include <cmath>
 
-using namespace std;
 namespace py = boost::python;
 
-void FixPair::prepareParameters(string handle, std::function<float (float, float)> fillFunction, std::function<float (float)> processFunction, bool fillDiag, std::function<float ()> fillDiagFunction) {
-    vector<float> &preProc = *paramMap[handle];
-    vector<float> *postProc = &paramMapProcessed[handle];
+void FixPair::prepareParameters(std::string handle,
+                                std::function<float (float, float)> fillFunction,
+                                std::function<float (float)> processFunction,
+                                bool fillDiag,
+                                std::function<float ()> fillDiagFunction)
+{
+    std::vector<float> &preProc = *paramMap[handle];
+    std::vector<float> *postProc = &paramMapProcessed[handle];
     int desiredSize = state->atomParams.numTypes;
 
     *postProc = preProc;
@@ -24,57 +28,58 @@ void FixPair::prepareParameters(string handle, std::function<float (float, float
 
 }
 
-void FixPair::prepareParameters(string handle, std::function<float (float)> processFunction) {
-   // vector<float> &array = *paramMap[handle];
-   // vector<float> *preproc = &paramMapPreproc[handle];
+void FixPair::prepareParameters(std::string handle,
+                                std::function<float (float)> processFunction)
+{
+   // std::vector<float> &array = *paramMap[handle];
+   // std::vector<float> *preproc = &paramMapPreproc[handle];
    // int desiredSize = state->atomParams.numTypes;
     //ensureParamSize(array);
    // *preproc = array;
-    vector<float> &preProc = *paramMap[handle];
-    vector<float> *postProc = &paramMapProcessed[handle];
+    std::vector<float> &preProc = *paramMap[handle];
+    std::vector<float> *postProc = &paramMapProcessed[handle];
     int desiredSize = state->atomParams.numTypes;
 
     *postProc = preProc;
     ensureParamSize(*postProc);
     SquareVector::check_populate<float>(postProc, desiredSize);
     SquareVector::process<float>(postProc, desiredSize, processFunction);
-    
-
 }
 
-void FixPair::prepareParameters(string handle, std::function<float (int, int)>  fillFunction) {
-    //vector<float> &array = *paramMap[handle];
+void FixPair::prepareParameters(std::string handle,
+                                std::function<float (int, int)>  fillFunction)
+{
+    //std::vector<float> &array = *paramMap[handle];
     //int desiredSize = state->atomParams.numTypes;
     //ensureParamSize(array);
-    vector<float> &preProc = *paramMap[handle];
-    vector<float> *postProc = &paramMapProcessed[handle];
+    std::vector<float> &preProc = *paramMap[handle];
+    std::vector<float> *postProc = &paramMapProcessed[handle];
     int desiredSize = state->atomParams.numTypes;
 
     *postProc = preProc;
     ensureParamSize(*postProc);
     SquareVector::populate<float>(postProc, desiredSize, fillFunction);
-
 }
 
 
 
-void FixPair::ensureParamSize(vector<float> &array) {
-
+void FixPair::ensureParamSize(std::vector<float> &array)
+{
     int desiredSize = state->atomParams.numTypes;
     if (array.size() != desiredSize*desiredSize) {
-        vector<float> newVals = SquareVector::copyToSize(
+        std::vector<float> newVals = SquareVector::copyToSize(
                 array,
                 sqrt((double) array.size()),
                 state->atomParams.numTypes
                 );
-        vector<float> *asPtr = &array;
+        std::vector<float> *asPtr = &array;
         *(&array) = newVals;
     }
 }
 
 void FixPair::ensureOrderGivenForAllParams() {
     for (auto it=paramMap.begin(); it!=paramMap.end(); it++) {
-        string handle = it->first;
+        std::string handle = it->first;
         if (find(paramOrder.begin(), paramOrder.end(), handle) == paramOrder.end()) {
             mdError("Order for all parameters not specified");
         }
@@ -88,30 +93,30 @@ void FixPair::sendAllToDevice() {
     }
     paramsCoalesced = GPUArrayDeviceGlobal<float>(totalSize);
     int runningSize = 0;
-    for (string handle : paramOrder) {
-        vector<float> &vals = paramMapProcessed[handle];
+    for (std::string handle : paramOrder) {
+        std::vector<float> &vals = paramMapProcessed[handle];
         paramsCoalesced.set(vals.data(), runningSize, vals.size());
         runningSize += vals.size();
     }
 }
 
-bool FixPair::setParameter(string param,
-                           string handleA,
-                           string handleB,
+bool FixPair::setParameter(std::string param,
+                           std::string handleA,
+                           std::string handleB,
                            double val)
 {
     int i = state->atomParams.typeFromHandle(handleA);
     int j = state->atomParams.typeFromHandle(handleB);
     if (paramMap.find(param) != paramMap.end()) {
         int numTypes = state->atomParams.numTypes;
-        vector<float> &arr = *(paramMap[param]);
+        std::vector<float> &arr = *(paramMap[param]);
         ensureParamSize(arr);
         if (i>=numTypes or j>=numTypes or i<0 or j<0) {
-            cout << "Tried to set param " << param
+            std::cout << "Tried to set param " << param
                       << " for invalid atom types " << handleA
                       << " and " << handleB
                       << " while there are " << numTypes
-                      << " species." << endl;
+                      << " species." << std::endl;
             return false;
         }
         squareVectorRef<float>(arr.data(), numTypes, i, j) = val;
@@ -120,33 +125,33 @@ bool FixPair::setParameter(string param,
     return false;
 }
 
-void FixPair::initializeParameters(string paramHandle,
-                                   vector<float> &params) {
+void FixPair::initializeParameters(std::string paramHandle,
+                                   std::vector<float> &params) {
     ensureParamSize(params);
     paramMap[paramHandle] = &params;
-    paramMapProcessed[paramHandle] = vector<float>();
+    paramMapProcessed[paramHandle] = std::vector<float>();
 }
 
-string FixPair::restartChunkPairParams(string format) {
-  stringstream ss;
-  char buffer[128];
-  //ignoring format for now                                                                                                                               
-  for (auto it=paramMap.begin(); it!=paramMap.end(); it++) {
-    sprintf(buffer, "<parameter handle=\"%s\">", it->first.c_str());
-    ss << buffer << "\n";
-    for (int i = 0; i < it->second->size(); i++) {
-      for (int j = 0; j < it->second[i].size(); j++) {
-	ss << it->second[i][j] << "\n";
-      }
+std::string FixPair::restartChunkPairParams(std::string format) {
+    std::stringstream ss;
+    char buffer[128];
+    //ignoring format for now
+    for (auto it=paramMap.begin(); it!=paramMap.end(); it++) {
+        sprintf(buffer, "<parameter handle=\"%s\">", it->first.c_str());
+        ss << buffer << "\n";
+        for (int i = 0; i < it->second->size(); i++) {
+            for (int j = 0; j < it->second[i].size(); j++) {
+	            ss << it->second[i][j] << "\n";
+            }
+        }
+        /*
+        for (float x : *(it->second)) {
+            std::cout << "Parameter: " << x << std::endl;
+              ss << x << "\n";
+        }*/
+        ss << "</parameter>\n";
     }
-    /*
-    for (float x : *(it->second)) {
-      cout << "Parameter: " << x << endl;
-      ss << x << "\n";
-      }*/
-    ss << "</parameter>\n";
-  }
-  return ss.str();
+    return ss.str();
 }    
 
 void export_FixPair() {
