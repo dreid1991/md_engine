ee2b6828bc954ddd9af338f736cfff216efb385a
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2016-11-10 15:20:20 -0600

    TIP3P configuration from the end of summer

--- src/Fixes/FixRigid.h
+++ src/Fixes/FixRigid.h
@@ -2,21 +2,32 @@
 #ifndef FIXRIGID_H
 #define FIXRIGID_H
 
+#include "Python.h"
 #include "Fix.h"
+#include "FixBond.h"
 #include <boost/python.hpp>
 #include <boost/python/list.hpp>
 #include "GPUArrayDeviceGlobal.h"
 
-float4 settle_xs(float timestep, float4 com, float4 *xs_0, float4 *xs);
-float4 settle_vs(float timestep, float4 *vs_0, float4 *vs);
+//void settle_xs(float timestep, float3 com, float3 com1, float3 *xs_0, float3 *xs, float3 *fix_len);
+//void settle_vs(float timestep, float3 *vs_0, float3 *vs, float3 *xs, float *mass, float3 *fix_len);
+
+void export_FixRigid();
 
 class FixRigid : public Fix {
  private:
   GPUArrayDeviceGlobal<int4> waterIdsGPU;
   GPUArrayDeviceGlobal<float4> xs_0;
   GPUArrayDeviceGlobal<float4> vs_0;
+  GPUArrayDeviceGlobal<float4> dvs_0;
+  GPUArrayDeviceGlobal<float4> fs_0;
   GPUArrayDeviceGlobal<float4> com;
+  GPUArrayDeviceGlobal<float4> fix_len;
   std::vector<int4> waterIds;
+  std::vector<BondVariant> bonds;
+  std::vector<float4> invMassSums;
+  bool fourSet = false;
+  bool fourSite;
 
  public:
   FixRigid(SHARED(State), std::string handle_, std::string groupHandle_);
@@ -24,6 +35,11 @@ class FixRigid : public Fix {
   bool stepFinal();
   bool prepareForRun();
   void createRigid(int, int, int);
+  //void createRigidTIP4P(int, int, int, int);
+
+  std::vector<BondVariant> *getBonds() {
+    return &bonds;
+  }
 };
 
 #endif
