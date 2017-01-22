6ead643ab1c69008afead3d4bc4a60fa4b50ab0f
Daniel Reid <dreid1991@gmail.com>
2017-01-22 16:31:15 -0600

    updated atom ids list

--- python/setup.py
+++ python/setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup, Extension
 
-incDirsPy = '/home/danielreid/md_engine_ssages/core/src;/home/danielreid/md_engine_ssages/core/src/GPUArrays;/home/danielreid/md_engine_ssages/core/src/Integrators;/home/danielreid/md_engine_ssages/core/src/Fixes;/home/danielreid/md_engine_ssages/core/src/DataStorageUser;/home/danielreid/md_engine_ssages/core/src/Evaluators;/home/danielreid/md_engine_ssages/core/src/BondedForcers'.split(';')
+incDirsPy = '/home/danielreid/md_engine/core/src;/home/danielreid/md_engine/core/src/GPUArrays;/home/danielreid/md_engine/core/src/Integrators;/home/danielreid/md_engine/core/src/Fixes;/home/danielreid/md_engine/core/src/DataStorageUser;/home/danielreid/md_engine/core/src/Evaluators;/home/danielreid/md_engine/core/src/BondedForcers'.split(';')
 
 module1 = Extension('Sim',
-                    sources = ['/home/danielreid/md_engine_ssages/core/python/Sim.cpp'],
-                    library_dirs = ["/home/danielreid/md_engine_ssages/core"],
+                    sources = ['/home/danielreid/md_engine/core/python/Sim.cpp'],
+                    library_dirs = ["/home/danielreid/md_engine/core"],
                     libraries = ["Sim"],
                     include_dirs = incDirsPy + 
-                                    ["/software/cuda-8.0-el6-x86_64/include"],
-                    runtime_library_dirs = ["/home/danielreid/ssages_gpu_midway1/build/danmd/lib"],
+                                    ["/software/cuda-7.5-el7-x86_64/include"],
+                    runtime_library_dirs = ["/usr/local/lib"],
                     extra_compile_args = " -std=c++11 -fpic".split()
                     )
 
@@ -17,6 +17,6 @@ setup(name='Sim',
       description='A GPU-base Molecular Dynamics simulation engine',
       author='Daniel Reid',
       author_email='danielreid@uchicago.edu',
-      package_dir={ '': '/home/danielreid/md_engine_ssages/core/python' },
+      package_dir={ '': '/home/danielreid/md_engine/core/python' },
       ext_modules=[module1],
       )
