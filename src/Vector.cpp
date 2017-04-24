3a43c2dac2511162ae3644d7f3d4769190d55854
Daniel Reid <dreid1991@gmail.com>
2017-04-24 14:21:53 -0500

    exported vector dot

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -24,6 +24,7 @@ void export_Vector() {
         .def("dist", &Vector::dist<double>)
         .def("len", &Vector::len)
         .def("lenSqr", &Vector::lenSqr)
+        .def("dot", &Vector::dot)
         .def(self + self)
         .def(self - self)
         .def(self * self)
